3516382f8c41fe698f0a3298179b30479ed365a4
adeiskandarzulkarnaen <adeiskandarzulkarnaen@gmail.com>
2023-09-14 21:11:56 +0700

    change format latitude - longitude

--- src/service/SaveGeoJson.js
+++ src/service/SaveGeoJson.js
@@ -7,17 +7,35 @@ class SaveGeoJson {
     for (const data of geojsonData) {
       const { province, district, sub_district: subDistrict, village, border } = data
 
-      const borderString = JSON.stringify(border)
+      const borderString = JSON.stringify(border).replace(/\s/g, '');
+      let borderReversed;
 
+      if (borderString.includes('[[[')) {
+        borderReversed = this._reverseCoordinates3D(border);
+      } else if (borderString.includes('[[')){
+        borderReversed = this._reverseCoordinates2D(border);
+      }
+
+      const formatedBorder = JSON.stringify(borderReversed).replace(/\s/g, '')
       const query = {
         sql: `INSERT INTO region(province, district, sub_district, village, border) 
           VALUES (?, ?, ?, ?, ?)`,
-        values:[province, district, subDistrict, village, borderString]
+        values:[province, district, subDistrict, village, formatedBorder]
       }
 
       await this._pool.query(query);
       console.log(`Berhasil menambahkan Prov.${province} Kab.${district} Kec.${subDistrict} Kel.${village}`)
     }
+  }
+
+  _reverseCoordinates2D(coordinates) {
+    return coordinates.map(coord => [coord[1], coord[0]]);
+  }
+  
+  _reverseCoordinates3D(coordinates) {
+    return coordinates.map(subArray =>
+      subArray.map(coord => [coord[1], coord[0]])
+    );
   }
 }
 
